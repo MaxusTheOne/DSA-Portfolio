62611fa5379b331555d70f0e977afb630900d0cb
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-09-14 11:22:50 +0200

    Create SinglyLinkedList.js

---
+++ linkedList/SinglyLinkedList.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(data){
+
+    }
+
+    remove(data){
+
+    }
+
+    getFirst(){
+
+    }
+
+    getLast(){
+
+    }
+
+    getFirstNode(){
+
+    }
+
+    getNodeWith(data){
+
+    }
+
+    removeFirstNode(){
+
+    }
+
+}
+
+module.exports = SinglyLinkedList;
