6ceb7aac29d943ea08e039f7e08e93c126a62673
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-09-15 22:58:42 +0200

    Trying my best

--- linkedList/SinglyLinkedList.js
+++ linkedList/SinglyLinkedList.js
@@ -11,14 +11,46 @@ class SinglyLinkedList {
     }
 
     add(data){
+        let newNode = new Node(data);
+        if(this.head === null){
+            this.head = newNode;
+
+        }else{
+            let current = this.head;
+            while(current.next !== null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
 
     }
 
     remove(data){
+        if(this.head === null){
+            return;
+        }
+        if(this.head.value === data){
+            this.head = this.head.next;
+            return;
+        }
+        let current = this.head;
+        while(current.next !== null){
+            if(current.next.value === data){
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
 
     }
 
     getFirst(){
+
+        if (this.next === null) {
+            return this.head;
+        }else {
+            return this.next.getFirst();
+        }
 
     }
 
@@ -39,5 +71,4 @@ class SinglyLinkedList {
     }
 
 }
-
-module.exports = SinglyLinkedList;
+export default SinglyLinkedList;
