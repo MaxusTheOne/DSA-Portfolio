b64bf32133b9258c30dc0fd4d0d47472ebc2510b
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2024-10-27 18:24:07 +0100

    ShuntingYard type here :)

---
+++ shuntingYard/rpnCalculator.js
@@ -0,0 +1,38 @@
+function RpnCalculator(arr) {
+    const calcStack = [];
+ 
+    while (arr.length > 0) {
+        let element = arr.shift();
+        if (typeof element === "number") {
+            calcStack.push(element);
+        } else {
+            operation(element, calcStack);
+        }
+    }
+    return calcStack[0];
+}
+ 
+function operation(operation, stack) {
+    const b = stack.pop();
+    const a = stack.pop();
+    switch (operation) {
+        case "+":
+            stack.push(a + b);
+            break;
+        case "-":
+            stack.push(a - b);
+            break;
+        case "*":
+            stack.push(a * b);
+            break;
+        case "/":
+        case "%":
+            stack.push(a / b);
+            break;
+        case "^":
+            stack.push(a ** b);
+            break;
+        default:
+            break;
+    }
+}
